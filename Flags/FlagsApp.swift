ece04a1e22cb9433bb8a1e81d5cf3a50d75c73a1
NZhukov <nzhukov.rnd@gmail.com>
2024-03-17 14:04:54 +0400

    Initial Commit

---
+++ Flags/FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsApp.swift
+//  Flags
+//
+//  Created by Nikolay Zhukov on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
