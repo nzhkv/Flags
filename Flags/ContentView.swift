73a571395b5bbf7f382cca032f62247c8a980b51
NZhukov <nzhukov.rnd@gmail.com>
2024-03-18 11:48:17 +0400

    done

--- Flags/ContentView.swift
+++ Flags/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = FlagsViewModel()
+    @State private var currentFlagOffset = CGFloat.zero
+    @State private var nextFlagOffset: CGFloat = -160
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            ZStack {
+                Text(viewModel.flags[viewModel.currentFlagIndex])
+                    .font(.system(size: 100))
+                    .offset(y: currentFlagOffset)
+                
+                Text(viewModel.flags[viewModel.nextFlagIndex])
+                    .font(.system(size: 100))
+                    .offset(y: nextFlagOffset)
+            }
+            .frame(width: 160, height: 160)
+            .background(Color.clear)
+            .overlay(
+                Rectangle()
+                    .strokeBorder(Color.black, lineWidth: 5)
+            )
+            .clipped()
+            
+            Spacer()
+            
+            Button("Hit me!") {
+                let animationDuration = 1.5
+                let flagMovementDistance: CGFloat = 160
+                
+                nextFlagOffset = flagMovementDistance
+                
+                withAnimation(.easeInOut(duration: animationDuration)) {
+                    currentFlagOffset = -flagMovementDistance
+                    nextFlagOffset = 0
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + animationDuration) {
+                    viewModel.changeFlag()
+                    currentFlagOffset = 0
+                    nextFlagOffset = flagMovementDistance
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            .padding(.horizontal, 20)
         }
-        .padding()
     }
 }
 
--- Flags/FlagsViewModel.swift
+++ Flags/FlagsViewModel.swift
@@ -5,4 +5,19 @@
 //  Created by Nikolay Zhukov on 17.03.2024.
 //
 
-import Foundation
+import SwiftUI
+
+class FlagsViewModel: ObservableObject {
+    
+    @Published var currentFlagIndex = 0
+    @Published var nextFlagIndex = 1
+    
+    let flags = ["🇦🇫", "🇦🇽", "🏴‍☠️", "🇦🇱", "🇩🇿", "🇦🇩"]
+    
+    func changeFlag() {
+        currentFlagIndex = (currentFlagIndex + 1) % flags.count
+        nextFlagIndex = (currentFlagIndex + 1) % flags.count
+    }
+    
+}
+
